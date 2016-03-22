79336293df0315fb3fb04c48ab516194cabbc5bf
Leran <leranf#gmail.com>
2016-03-22 14:29:57 -0700

    boya work

--- client/app/header.js
+++ client/app/header.js
@@ -5,7 +5,6 @@ import { fbLogin } from '../auth/authActions';
 
 const Header = () => (
   <AppBar title="Name This">
-    <button onClick={fbLogin}> Facebook button </button>
     <Link to="/trip"><Tab label="My Trips" /></Link>
     <Link to="/home"><Tab label="Home" /></Link>
   </AppBar>
