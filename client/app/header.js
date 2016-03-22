d4f2e71e5c71a6fb5efe598866317db88da6b176
boyajay <walkingonglass@gmail.com>
2016-03-21 18:26:34 -0700

    add non-functioning button for FB

--- client/app/header.js
+++ client/app/header.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 
 const Header = () => (
   <AppBar title="Name This">
+  	<button> Facebook button </button>
     <Link to="/trip"><Tab label="My Trips" /></Link>
     <Link to="/home"><Tab label="Home" /></Link>
   </AppBar>
